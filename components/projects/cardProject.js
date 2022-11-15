f45ca0d426d002fb6c66f3d1d2e2944f99d33449
A2nis <adonisfernandezr@gmail.com>
2022-11-15 17:11:38 -0500

    updated project card component

--- components/projects/cardProject.js
+++ components/projects/cardProject.js
@@ -12,9 +12,14 @@ export default function CardProject({
   link,
 }) {
   return (
-    <div className="flex flex-col bg-slate-100 dark:bg-slate-800 pb-4 gap-1 rounded-b-xl shadow-xl">
+    <div className="flex flex-col bg-slate-100 dark:bg-slate-800 pb-4 gap-1 rounded-t-lg rounded-b-xl shadow-lg hover:shadow-black/40 dark:hover:shadow-gray-600/40">
       <div className="relative w-80 h-52 ">
-        <Image src={picture} alt="picture" layout="fill" />
+        <Image
+          src={picture}
+          alt="picture"
+          layout="fill"
+          className="rounded-t-lg"
+        />
         <div className="absolute bottom-2 right-0 bg-gray-500 font-semibold text-white px-1">
           {link ? (
             <Link href={link}>
@@ -28,18 +33,18 @@ export default function CardProject({
       <div className="px-3 py-2">
         <div className="pb-3">
           <h1 className="font-semibold text-lg dark:text-gray-200">{name}</h1>
-          <div className="flex gap-3">
+          <div className="flex gap-1">
             {tags.map((t) => (
               <div
                 key={t}
-                className="rounded-sm bg-slate-300 px-1 cursor-default"
+                className="flex rounded-sm bg-slate-300 px-1 cursor-default"
               >
-                <span className="font-serif">{t}</span>
+                <span className="text-xs font-bold text-gray-900/70">{t}</span>
               </div>
             ))}
           </div>
         </div>
-        <div className="space-x-4 ">
+        <div className="space-x-2 ">
           <Btn text="Preview" href={preview} />
           <Btn text="Code" href={code} />
         </div>
